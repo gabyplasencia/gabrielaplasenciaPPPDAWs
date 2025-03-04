a6d28bfe7b671b4f3dfb45232096e88a501d38b9
Gabriela Plasencia <gabyplasencia1995@gmail.com>
2025-03-04 15:32:21 +0000

    se creo la estructura del componente Register.jsx

--- front-usuarios/src/pages/Register.jsx
+++ front-usuarios/src/pages/Register.jsx
@@ -2,9 +2,24 @@
 const Register = () => {
     
     return (
-        <div className="register">
-            <h1>Register</h1>
-        </div>
+        <section className="main-wrapper">
+            <form action="/" className="form">
+                <h1 className="form__title">REGISTER</h1>
+                <div className="form__field-wrapper">
+                    <label htmlFor="email" className="form__label">Email:</label>
+                    <input type="email" name="email" id="email" className="form__input"/>
+                </div>
+                <div className="form__field-wrapper">
+                    <label htmlFor="username" className="form__label">Username:</label>
+                    <input type="username" name="username" id="username" className="form__input"/>
+                </div>
+                <div className="form__field-wrapper">
+                    <label htmlFor="password" className="form__label">Password:</label>
+                    <input type="password" name="password" id="password" className="form__input"/>
+                </div>   
+                <button className="form__submit-btn">REGISTER</button> 
+            </form>
+        </section>
     )
 }
 
