843727bd8ae7c288ed32e89caf1c92f0bc88ca13
Gabriela Plasencia <gabyplasencia1995@gmail.com>
2025-03-04 14:45:42 +0000

    issue: Crear los estilos generales (vistas usuarios) #6

--- front-usuarios/src/App.jsx
+++ front-usuarios/src/App.jsx
@@ -1,10 +1,26 @@
-
+import { Suspense } from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
 
   return (
     <>
-
+      <h1>LOGO</h1>
+      <span>AVATAR</span>
+      <Suspense fallback="loading">
+        <BrowserRouter>
+            
+            <div role="main">
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>         
+            </div>
+            
+        </BrowserRouter>
+    </Suspense>
     </>
   )
 }
