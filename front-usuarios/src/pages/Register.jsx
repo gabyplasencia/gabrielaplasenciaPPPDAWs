881f3be31c3c06c702415fe90f7084ec6811368c
Gabriela Plasencia <gabyplasencia1995@gmail.com>
2025-03-10 18:34:44 +0000

    agregue el required al register y cambie auth.php

--- front-usuarios/src/pages/Register.jsx
+++ front-usuarios/src/pages/Register.jsx
@@ -7,15 +7,15 @@ const Register = () => {
                 <h1 className="form__title">REGISTER</h1>
                 <div className="form__field-wrapper">
                     <label htmlFor="email" className="form__label">Email:</label>
-                    <input type="email" name="email" id="email" className="form__input"/>
+                    <input type="email" name="email" id="email" className="form__input" required/>
                 </div>
                 <div className="form__field-wrapper">
-                    <label htmlFor="username" className="form__label">Username:</label>
-                    <input type="username" name="username" id="username" className="form__input"/>
+                    <label htmlFor="name" className="form__label">Username:</label>
+                    <input type="name" name="name" id="name" className="form__input" required/>
                 </div>
                 <div className="form__field-wrapper">
                     <label htmlFor="password" className="form__label">Password:</label>
-                    <input type="password" name="password" id="password" className="form__input"/>
+                    <input type="password" name="password" id="password" className="form__input" required/>
                 </div>   
                 <button className="regular-bnt">REGISTER</button> 
             </form>
