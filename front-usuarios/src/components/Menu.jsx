e3b97dfa537130c893728cdbab1ccbbfab2835fc
Gabriela Plasencia <gabyplasencia1995@gmail.com>
2025-03-21 19:28:02 +0000

    separe el menu y lo volvi un componente que puedo llamar segun la vista

---
+++ front-usuarios/src/components/Menu.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+
+const Menu = () => {
+    
+    useEffect(() => {
+        document.documentElement.style.setProperty('--menu-bg-color', 'rgb(255, 211, 211)');
+        document.documentElement.style.setProperty('--menu-border-color', 'rgb(94, 12, 12)');
+    }, []);
+
+    return (
+        <section className="menu">
+            <button className="menu__btn" aria-label="open menu">
+            {/* cambiar el src a uno que coincida con el avatar del ususario */}
+                <img src="/avatars/kitty.png" alt="profile photo" aria-hidden="true"/>
+            </button>
+            <h4>MENU</h4>
+        </section>
+    )
+}
+
+export default Menu
