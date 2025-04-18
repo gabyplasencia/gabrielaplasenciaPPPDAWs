69b944880a6ad47982e8536a9273ecc33fd88ba6
Gabriela Plasencia <gabyplasencia1995@gmail.com>
2025-04-18 17:18:40 +0100

    issues #13 #57 el menu ya muestra el modal y estan la vista del modal principal

--- front-usuarios/src/components/Menu.jsx
+++ front-usuarios/src/components/Menu.jsx
@@ -1,27 +1,58 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { Link } from "react-router-dom";
 
 const Menu = () => {
     const { user } = useAuth();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     useEffect(() => {
-        document.documentElement.style.setProperty('--menu-bg-color', 'rgb(255, 211, 211)');
-        document.documentElement.style.setProperty('--menu-border-color', 'rgb(94, 12, 12)');
-    }, []);
+      document.documentElement.style.setProperty('--menu-bg-color', 'rgb(255, 211, 211)');
+      document.documentElement.style.setProperty('--menu-border-color', 'rgb(94, 12, 12)');
+  }, []);
 
     if (!user) return null;
     const avatarPath = "/assets/avatars/" + user.avatar;
 
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <>
+          {isMenuOpen && (
+              <div 
+                className="overlay"
+                onClick={toggleMenu}
+                aria-hidden="true"
+              />
+          )}
           <section className="menu">
-          <button className="menu__btn" aria-label="open menu">
-            <img src={avatarPath} alt="profile photo" aria-hidden="true"/>
-          </button>
-          <h4>MENU</h4>
-          <Link to="/logout">Cerrar sesión</Link>
-        </section>
+              <button className="menu__btn"
+                      aria-label="open menu"
+                      onClick={toggleMenu}
+                      aria-expanded={isMenuOpen}>
+                  <img src={avatarPath} alt="profile photo" aria-hidden="true"/>
+              </button>
+              <h4>MENU</h4>
+          </section>
+          <section className={`modal__container ${isMenuOpen ? 'isOpen' : ''}`}
+                    aria-hidden={!isMenuOpen}
+          > 
+              <div className="modal main-wrapper ">
+                  <h2 className="modal__title">MENU</h2>
+                  <div className="modal__music-wrapper">
+                      <p className="modal__music-text">MUSIC</p>
+                      <button className="regular-btn modal__music-btn" aria-label="turn on music">ON</button>
+                  </div>
+                  <button className="regular-btn">SCORES</button>
+                  <button className="regular-btn">AVATAR</button>
+                  <button className="regular-btn">HELP</button>
+                  <Link className="modal__log-out regular-btn" to="/logout">LOG OUT</Link>
+                  <img className="modal__close mode__icon" src="/assets/icons/close-icon.png" alt="close icon" aria-label="close menu" onClick={toggleMenu}/>
+              </div>
+              
+          </section>
         </>
 
     )
