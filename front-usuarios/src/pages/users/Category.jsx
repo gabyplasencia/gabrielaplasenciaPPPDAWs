3bdab68b71441ad40895747cfb11c6c6469349e3
Gabriela Plasencia <gabyplasencia1995@gmail.com>
2025-04-18 18:51:13 +0100

    cambie a por link para que mneje mejor el cambio de ruta

--- front-usuarios/src/pages/users/Category.jsx
+++ front-usuarios/src/pages/users/Category.jsx
@@ -1,12 +1,13 @@
 import Menu from "../../components/Menu";
+import { Link } from "react-router-dom";
 
 const Category = () => {
     return (
         <>
         <Menu />
         <div className="category">
-            <a href="/flags" className="game-btn">FLAGS</a>
-            <a href="/capitals" className="game-btn">CAPITALS</a>
+            <Link to="/flags" className="game-btn category__btn">FLAGS</Link>
+            <Link to="/capitals" className="game-btn category__btn">CAPITALS</Link>
         </div>
         </>
     )
